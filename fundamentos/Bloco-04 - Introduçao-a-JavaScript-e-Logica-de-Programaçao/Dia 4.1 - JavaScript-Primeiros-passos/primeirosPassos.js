42ee4d7b1dec2a47cdf07bbcc70f16e6d73f531c
PatriciaDivi <patriciavianad@gmail.com>
2021-11-26 18:27:39 -0300

    Alterações no Bloco-03 | Adicionado Bolo-04 | Dia 4.1 | Dia 4.2

---
+++ fundamentos/Bloco-04 - Introduçao-a-JavaScript-e-Logica-de-Programaçao/Dia 4.1 - JavaScript-Primeiros-passos/primeirosPassos.js
@@ -0,0 +1,22 @@
+let a = 10
+let b = 20
+
+// Adição (a + b)
+let resul = a + b
+console.log(a + b)
+
+// Subtração (a - b)
+let resul01 = a - b
+console.log(a - b)
+
+// Multiplicação (a * b)
+let mult = a * b
+console.log(a * b)
+
+// Divisão (a / b)
+let div = a / b
+console.log(a / b)
+
+// Módulo (a % b)
+let mod = a % b
+console.log(a % b)
